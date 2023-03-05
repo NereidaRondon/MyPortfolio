e55194f0d420eb08b1028bfc0d94b379cabc67c7
nereidarondon <nereida.rondon5@gmail.com>
2023-03-05 00:25:46 -0500

    removed keyboard audio

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-let type = new Audio('music/keyboard-typing-139083.mp3');
-$(document).ready(type.play()); 
+
 
 setTimeout(() => {
   $('.typewriter1').css('border-right', 'none');
   $('.typewriter').css('visibility', 'visible');
   
-}, "2025");
+}, "3000");
 
